475f371cda93baf8e3c2600fa555ae0c1d21a3b4
Spacemarioedition <spacemarioedition@hotmail.com>
2025-12-13 17:51:18 -0600

    bewabewa

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,8 +51,8 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || '0/0/0' }}</p>
+                            <div class="type-title-sm">placedOn</div>
+                            <p>{{ level.placedOn || '0/0/0' }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>
